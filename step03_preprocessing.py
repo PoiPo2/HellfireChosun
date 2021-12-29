c7dd0219415370781c492c365da0c6c91a762b54
PoiPo2 <kimmc0316@gmail.com>
2021-12-29 10:10:36 +0900

    모델 입력에 필요한 형태로 변환시켜주기 위한 전처리 스크립트

--- step03_preprocessing.py
+++ step03_preprocessing.py
@@ -1,17 +1,15 @@
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.model_selection import train_test_split
-from tensorflow.keras.models import load_model, Sequential
-from tensorflow.keras.layers import *
-import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+import pickle
 import time
 
 
 """ 시퀀스 데이터의 주기(period)를 정의합니다. 단위는 일(day) 입니다. """
 """ period 또한 적절히 분배할 필요가 있습니다. """
-PERIOD = 1095
-FILENAME = f'training_metadata_{PERIOD}.npy'
+PERIOD = 30
+FILENAME = f'training_metadata_Mungyeong_{PERIOD}.npy'
 
 
 def getPeriod():
@@ -21,12 +19,15 @@ def getPeriod():
 if __name__ == '__main__':
     start_time = time.time()
     """ 전처리하고자 하는 csv 파일을 불러옵니다. """
-    data = pd.read_csv('./data/Gyeonggi_2_Suwon_renew_11315.csv', index_col='datetime')
+    # data = pd.read_csv('./data/Gyeongbuk_2_Gumi_renew_11315.csv', index_col='datetime')
+    data = pd.read_csv('./data/Gyeongbuk_3_Mungyeong_renew_11315.csv', index_col='datetime')
     # print(f'{data}\n{type(data)}')
 
     """ 모든 데이터 값들을 0과 1사이의 값으로 스케일링(Scaling) 합니다. """
     minmaxscaler = MinMaxScaler()
     scaled_data = minmaxscaler.fit_transform(data)
+    with open('./scaler.pickle', 'wb') as f:
+        pickle.dump(minmaxscaler, f)
 
     """ 시퀀스(Sequence) 데이터를 생성하기 위한 리스트를 정의합니다. """
     sequence_x, sequence_y = [], []
@@ -45,13 +46,16 @@ if __name__ == '__main__':
     # print(sequence_y[0], sequence_y.shape)
 
     """ 학습 및 테스트에 사용할 데이터셋을 구분 및 생성합니다. """
-    x_train, x_test, y_train, y_test = train_test_split(sequence_x, sequence_y, test_size=0.2)
+    x_train, x_test, y_train, y_test = train_test_split(sequence_x, sequence_y, test_size=0.15)
+    training_metadata = x_train, x_test, y_train, y_test
     # print(x_train.shape)
     # print(x_test.shape)
     # print(y_train.shape)
     # print(y_test.shape)
-    training_metadata = x_train, x_test, y_train, y_test
     """ 생성한 데이터셋을 .npy 파일로 저장합니다. """
     np.save(f'./{FILENAME}', training_metadata)
     print(f'"{FILENAME}" is saved.')
     print(f'runtime is {round(time.time() - start_time, 3)} seconds.')
+    """ 디버그(debug)를 위해 사용합니다. """
+    print(min(data['avg_tmp']))
+    print(max(data['avg_tmp']))
